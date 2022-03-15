fd57f0b06995522fa9ca18baecc71f3464858be1
hormorht <hormorht@gmail.com>
2022-03-15 14:59:57 -0700

    Low Level language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *  * Return: Always 0 (Success)
- *  */
+ * main - Entry point
+ * Return: return 0 after printing
+ */
 int main(void)
 {
 	int i;

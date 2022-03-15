77a9561b595776195e08b09401d6136976b9ccb6
hormorht <hormorht@gmail.com>
2022-03-15 09:03:33 -0700

    Low Level Language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *
  * Return: 0 after printing function
  */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
